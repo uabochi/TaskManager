7e7438069a0377273f6a6e4584ec6f65a059aff3
uabochi <uabochi@gmail.com>
2024-11-28 23:29:09 +0100

    Formatted code

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,9 +2,19 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    username: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String, 
+        required: true },
 });
 
 // Hash password before saving
